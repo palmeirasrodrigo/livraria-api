2ac3a5121720b86f82578728d804c69b725c3d07
palmeirasrodrigo <palmeirasrodrigo@hotmail.com>
2021-03-07 10:55:03 -0300

    POST para Livro

--- src/main/java/com/estudos/rodrigo/service/LivroService.java
+++ src/main/java/com/estudos/rodrigo/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.estudos.rodrigo.domain.Categoria;
 import com.estudos.rodrigo.domain.Livro;
 import com.estudos.rodrigo.repositories.LivroRepository;
 
@@ -26,5 +27,24 @@ public class LivroService {
 	public List<Livro> findAll(Integer id_cat) {
 		categoriaService.findById(id_cat);
 		return livroRepository.findAllByCategoria(id_cat);
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return livroRepository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return livroRepository.save(obj);
 	}
 }
